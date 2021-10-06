aabb328871d586b1dcb6301015e9b9c70ea3524a
Vladimir-Shane <VlaShin1986@gmail.com>
2021-10-06 21:56:44 +0300

    Create a new repository

---
+++ index.js
@@ -0,0 +1,9 @@
+import './js/module1-1.js'
+// import './js/module1-2.js'
+
+// import './js/module2-1-arrays.js'
+// import './js/module2-2-functions.js'
+
+
+
+// import './js/tasks.js'
---
+++ js/module1-1.js
@@ -0,0 +1,13 @@
+/*
+ * Напиши скрипт который просит пользователя ввести число и степень,
+ * возводит число в эту степень и выводит результат в консоль
+ */
+
+let base = prompt('Enter the base')
+base = Number(base)
+// console.log(typeof base);
+let power = prompt('Enter the power');
+power = Number(power)
+let result = Math.pow(base, power);
+console.log(result);
+alert(`Your result is : ${result}`);
